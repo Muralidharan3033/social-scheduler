923c14fd7b468d9d607f391fb5b72f69c0ec1f68
Muralidharan3033 <muralimala2005@gmail.com>
2026-07-25 11:23:38 +0530

    created basic express server

---
+++ server/server.ts
@@ -0,0 +1,34 @@
+import dns from "node:dns";
+
+// Force Google DNS (fixes MongoDB Atlas SRV lookup issues on some networks)
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+// Database connection
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+    res.send("Server is Live!");
+});
+
+// Global Error Handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err);
+    res.status(500).send(err?.response?.data?.message || err?.message);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
